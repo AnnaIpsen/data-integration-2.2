d578946143e027262bc7fc60e4b72d955c09fc52
Anna-1902 <annaipsen19@gmail.com>
2023-04-19 21:13:35 +0200

    vue widget in html and functions to display info from api + style

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,24 +1,97 @@
-let getCities = document.getElementById('getCities')
-let citiesOutput = document.getElementById('cities')
-let url = 'http://localhost:3000'
 
-async function getInfo() {
-    let res = await fetch(url + '/cities', {method:'GET'})
-    let data = await res.json()
-    let cities = data.cities
+const cities = Vue.createApp({
+    // root component, setting up data, methods
+    data() {                            // create data function
+        return {                        // that returns an object
+            citiesList: [],
+            countriesList: [],
+            showModal: false,
+            form: {
+                name: '',
+                countrycode: '',
+                district: '',
+                population: ''
+            },
+            selectedCountry: ''
+        }
+    },
+    methods: {                          // component methods
+        async getCities() {
+            let list = await fetch('http://localhost:3000/cities');
+            let finalist = await list.json();
+            this.citiesList = finalist.cities;
+        },
+        async getCountries() {
+            let list = await fetch('http://localhost:3000/countries');
+            let finalist = await list.json();
+            this.countriesList = finalist.countries;
+        },
+        async postCity(e){
+            e.preventDefault()
+            console.log(JSON.stringify(this.form))
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(this.form)
+            };
 
-    citiesOutput.innerHTML = ''
+            await fetch('http://localhost:3000/cities', requestOptions)
+                .then(()=>{
+                    alert('City was added successfully to database')
+                })
+                .then(()=> {
+                    //this.getCities()
+                    //this.showModal = false
+                })
+                .catch(()=>{
+                    alert('Some error occurred ')
+                })
 
+        },
+        async deleteCity(id){
+            await fetch(`http://localhost:3000/cities/${id}`, { method: 'DELETE' })
+                .then(()=>{
+                    alert('City was succesfully deleted')
+                })
+                .then(()=> this.getCities())
+                .catch(()=>{
+                    alert('Some error occurred ')
+                })
+        },
 
-    for (let i = 0; i < cities.length; i++) {
-        citiesOutput.innerHTML += `<div> 
-                <h3>${cities[i].name}</h3>
-                <p>Countrycode: ${cities[i].countrycode}</p>
-                <p>District: ${cities[i].district}</p>
-                <p>Population: ${cities[i].population}</p>
-                </div>`
+        async getCityByCountry(code) {
+            let list = await fetch(`http://localhost:3000/citiescountry/${code}`);
+            let finalist = await list.json();
+            this.citiesList = finalist.cities;
+        }
+
+    },
+    mounted: function () {
+        this.getCities();         // will execute at pageload
+        this.getCountries();
     }
+});
 
-}
+cities.mount('#cities');
 
-getCities.addEventListener('click', getInfo)
+const world = Vue.createApp({
+    // root component, setting up data, methods
+    data() {                            // create data function
+        return {                        // that returns an object
+            countriesList: [],
+        }
+    },
+    methods: {                          // component methods
+        async getCountries() {
+            let list = await fetch('http://localhost:3000/countries');
+            let finalist = await list.json();
+            this.countriesList = finalist.countries;
+        }
+
+    },
+    mounted: function () {
+        this.getCountries();             // will execute at pageload
+    }
+});
+
+world.mount('#world');          // refers to selector in page
