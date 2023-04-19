f4d22a50b3f9d5b3c194634752b4e6d74339c3dc
Anna-1902 <annaipsen19@gmail.com>
2023-04-19 21:14:40 +0200

    routes to get countries, city by country and delete city + get all countries

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,19 @@ const conw = require('../controllers/controllersworld')
  * Setup REST endpoints
  */
 
+router.get('/countries', conw.getCountries, function (req, res) {
+  // variable from middleware
+  res.json({ countries: res.locals.countries });
+});
+
 // Getting cities
 router.get('/cities', conw.getCities, function (req, res) {
+  res.json({ cities: res.locals.cities });
+});
+
+// Getting cities by country
+router.get('/citiescountry/:country', conw.getCitiesByCountry, function (req, res) {
+  // variable from middleware
   res.json({ cities: res.locals.cities });
 });
 
@@ -18,18 +29,19 @@ router.post('/cities', conw.postCity, function (req, res) {
 });
 
 // Getting city
-router.get('/cities:city', function (req, res) {
-
+router.get('/cities/:city', conw.getCity, function (req, res) {
+  res.json({ city: res.locals.city });
 });
 
 // Update city
-router.patch('/cities:city', function (req, res) {
+router.patch('/cities/:city', function (req, res) {
 
 });
 
 // Delete city
-router.delete('/cities:city', function (req, res) {
-
+router.delete('/cities/:city', conw.deleteCity, function (req, res) {
+  let msg = `deleted ${res.locals.deleted} city`;
+  res.status(201).json({ message: msg });
 });
 
 module.exports = router;
